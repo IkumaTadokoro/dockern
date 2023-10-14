1359c34cd873754cc59affe2cb8830c0fcac766c
ikuma-t <tadokorodev@gmail.com>
2023-10-14 18:06:13 +0900

    feat: dockernコマンドを実装

--- deps.ts
+++ deps.ts
@@ -1,3 +1,4 @@
 // cliffy
 export { Command } from "https://deno.land/x/cliffy@v1.0.0-rc.3/command/mod.ts";
 export { Checkbox } from "https://deno.land/x/cliffy@v1.0.0-rc.3/prompt/checkbox.ts";
+export { Confirm } from "https://deno.land/x/cliffy@v1.0.0-rc.3/prompt/confirm.ts";
--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-import { Checkbox, Command } from "./deps.ts";
+import { Checkbox, Command, Confirm } from "./deps.ts";
 
 async function listDockerImages() {
   const process = Deno.run({
@@ -31,6 +31,10 @@ async function deleteDockerImages(images: string[]) {
       console.log(`Deleted image: ${image}`);
     }
   }
+}
+
+async function confirmAction(message: string): Promise<boolean> {
+  return await Confirm.prompt(message); // Update this line
 }
 
 async function listDockerVolumes(): Promise<string[]> {
@@ -107,6 +111,67 @@ new Command()
       await deleteDockerVolumes(selectedVolumes);
     } else {
       console.log("No volumes selected for deletion.");
+    }
+  })
+  .command("dockern")
+  .description("Manage docker images and volumes.")
+  .action(async () => {
+    let selectedImages: string[] = [];
+    let selectedVolumes: string[] = [];
+
+    // Confirm and Select Images to delete
+    const confirmImageDeletion = await confirmAction(
+      "Do you want to delete images?",
+    );
+    if (confirmImageDeletion) {
+      const images = await listDockerImages();
+      if (images.length > 0) {
+        selectedImages = await Checkbox.prompt<string>({
+          message: "Select images to delete",
+          options: images.map((image) => ({ name: image, value: image })),
+        });
+      } else {
+        console.log("No images found.");
+      }
+    }
+
+    // Confirm and Select Volumes to delete
+    const confirmVolumeDeletion = await confirmAction(
+      "Do you want to delete volumes?",
+    );
+    if (confirmVolumeDeletion) {
+      const volumes = await listDockerVolumes();
+      if (volumes.length > 0) {
+        selectedVolumes = await Checkbox.prompt<string>({
+          message: "Select volumes to delete",
+          options: volumes.map((volume) => ({ name: volume, value: volume })),
+        }) as string[];
+      } else {
+        console.log("No volumes found.");
+      }
+    }
+
+    // If no images or volumes are selected, exit
+    if (selectedImages.length === 0 && selectedVolumes.length === 0) {
+      console.log("No images or volumes selected for deletion. Exiting.");
+      return;
+    }
+
+    // Confirm the action
+    if (selectedImages.length > 0) {
+      console.log("Selected images for deletion:", selectedImages.join(", "));
+    }
+    if (selectedVolumes.length > 0) {
+      console.log("Selected volumes for deletion:", selectedVolumes.join(", "));
+    }
+    const confirmDeletion = await confirmAction("Confirm deletion?");
+    if (confirmDeletion) {
+      if (selectedImages.length > 0) await deleteDockerImages(selectedImages); // イメージが選択された場合のみ削除
+      if (selectedVolumes.length > 0) {
+        await deleteDockerVolumes(selectedVolumes); // ボリュームが選択された場合のみ削除
+      }
+    } else {
+      console.log("Deletion cancelled.");
     }
   })
   .parse(Deno.args);
